9516513a6be9bd066a7d92d9897aa7f1e1cb5a17
michal <michal@localhost.localdomain>
2015-05-11 18:35:43 +0200

    Maked sections of commands

--- basic_config/basic_script.sh
+++ basic_config/basic_script.sh
@@ -7,13 +7,22 @@ echo
 
 aptitude install mc moc zsh
 
+#=================================================================
+# MOC 
 mkdir ~/.moc
 cp ~/repo/linux_stuff/basic_config/config_moc ~/.moc/config
+#=================================================================
 
+#=================================================================
+# Midnight Commander
 mkdir ~/.local/share/mc/skins
 cp ~/repo/linux_stuff/basic_config/mc.ext ~/.config/mc/mc.ext
 cp ~/repo/linux_stuff/basic_config/darkcourses_green.ini ~/.local/share/mc/skins/
+#=================================================================
 
+#=================================================================
+# Z-shell
 cp ~/repo/linux_stuff/basic_config/hide.zshrc ~/.zshrc
 chsh -s /bin/zsh
+#=================================================================
 
