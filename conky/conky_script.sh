4456aa95d75949671d9f68e4ee519d717d4ccaa8
michal <michal.dudek1995@gmail.com>
2015-05-13 10:37:38 +0200

    Added creating dir and cloning repo mechanism

--- conky/conky_script.sh
+++ conky/conky_script.sh
@@ -1,11 +1,37 @@
-# run this with sudo, not su!
-# at first type: cd ~/repo && git clone https://github.com/micdud1995/linux_stuff.git
+# run as a user!
 
-echo
-echo Configuration conky...
-echo
+clear
 
-aptitude install conky
+#=================================================================
+# Creating repo dir and cloning repository
+if [ ! -d ~/repo ] && [ ! -d ~/repo/linux_stuff ]; then
+	echo
+	echo =================================================================
+	echo Creating ~/repo/linux_stuff directory...
+	echo Cloning repository...
+	echo =================================================================
+	mkdir -p ~/repo
+	mkdir ~/repo/linux_stuff
+	cd ~/repo/linux_stuff
+	git clone https://github.com/micdud1995/linux_stuff.git
+else
+	echo
+	echo =================================================================
+	echo OK, ~/repo/linux_stuff already exists...
+	echo =================================================================
+fi
+#=================================================================
+
+#=================================================================
+echo
+echo =================================================================
+echo Configuration of conky...
+echo =================================================================
+echo
+#=================================================================
+
+sudo aptitude install conky
+exit
 
 #==============================================================
 # Copying .conkyrc
