6f44bb810907fa53d39ec921283c4f0a3837b4dd
michal <michal.dudek1995@gmail.com>
2015-05-13 10:30:22 +0200

    Added creating dir and cloning repo mechanism

--- basic_config/basic_script.sh
+++ basic_config/basic_script.sh
@@ -1,21 +1,45 @@
-# run this with sudo, not su!
-# at first type: cd ~/repo && git clone https://github.com/micdud1995/linux_stuff.git
+# run as a user!
+
+clear
+
+#=================================================================
+# Creating repo dir and cloning repository
+if [ ! -d ~/repo ] && [ ! -d ~/repo/linux_stuff ]; then
+	echo
+	echo =================================================================
+	echo Creating ~/repo/linux_stuff directory...
+	echo Cloning repository...
+	echo =================================================================
+	mkdir -p ~/repo
+	mkdir ~/repo/linux_stuff
+	cd ~/repo/linux_stuff
+	git clone https://github.com/micdud1995/linux_stuff.git
+else
+	echo
+	echo =================================================================
+	echo OK, ~/repo/linux_stuff already exists...
+	echo =================================================================
+fi
 
 echo
+echo =================================================================
 echo Configuration of moc player, midnight commander and zsh...
+echo =================================================================
 echo
+#=================================================================
 
-aptitude install mc moc zsh
+sudo aptitude install mc moc zsh
+exit
 
 #=================================================================
 # MOC 
-mkdir ~/.moc
+mkdir -p ~/.moc
 cp ~/repo/linux_stuff/basic_config/config_moc ~/.moc/config
 #=================================================================
 
 #=================================================================
 # Midnight Commander
-mkdir ~/.local/share/mc/skins
+mkdir -p ~/.local/share/mc/skins
 cp ~/repo/linux_stuff/basic_config/mc.ext ~/.config/mc/mc.ext
 cp ~/repo/linux_stuff/basic_config/darkcourses_green.ini ~/.local/share/mc/skins/
 #=================================================================
