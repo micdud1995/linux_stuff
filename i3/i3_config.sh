d1c0d3e6ab605b8208814ecf87a15bf55eceaf0a
michal <michal.dudek1995@gmail.com>
2015-05-18 13:40:55 +0200

    Added file

---
+++ i3/i3_config.sh
@@ -0,0 +1,36 @@
+clear
+
+#=================================================================
+# Creating repo dir and cloning repository
+if [ ! -d ~/repo ] && [ ! -d ~/repo/linux_stuff ]; then
+	echo
+	echo =================================================================
+	echo Creating ~/repo/linux_stuff directory...
+	echo Cloning repository...
+	echo =================================================================
+	mkdir -p ~/repo
+	mkdir ~/repo/linux_stuff
+	cd ~/repo/linux_stuff
+	git clone https://github.com/micdud1995/linux_stuff.git
+else
+	echo
+	echo =================================================================
+	echo OK, ~/repo/linux_stuff already exists...
+	echo =================================================================
+fi
+#=================================================================
+
+#=================================================================
+echo
+echo =================================================================
+echo Configuration of i3...
+echo =================================================================
+echo
+#=================================================================
+
+sudo aptitude install i3 dmenu lxrandr pavucontrol xbacklight
+
+#==============================================================
+# Copying config files from repo 
+cp ~/repo/linux_stuff/i3/hide.i3status.conf ~/.i3status.conf
+cp ~/repo/linux_stuff/i3/config ~/.i3/config
