83e686a3ca88fcd2dac6c78a662d69e3f8e0e4a3
michal <michal.dudek1995@gmail.com>
2015-05-19 18:01:49 +0200

    Added command that removes tmp file and command that converts two images into one

--- i3/i3lock_blurred.sh
+++ i3/i3lock_blurred.sh
@@ -1,5 +1,15 @@
 # Script that make blurred lockscreen from the current screen 
 
-scrot /tmp/screen.png
-convert /tmp/screen.png -scale 50% -scale 200% /tmp/screen.png
-i3lock -u -i /tmp/screen.png
+scrot ~/tmp/screen.png
+
+# Making screen blurred
+convert ~/tmp/screen.png -scale 50% -scale 200% ~/tmp/screen.png
+
+# Adding a image that will be in the center
+# [[ -f path/to/file.png ]] && convert ~/tmp/screen.png path/to/file.png -gravity center -composite -matte ~/tmp/screen.png
+
+# Activating i3lock
+i3lock -i ~/tmp/screen.png
+
+# Removing tmp file
+rm ~/tmp/screen.png
