ad37572d6d7bc25dafdcdb84f1caa4181576d16e
michal <michal.dudek1995@gmail.com>
2015-06-14 11:50:49 +0200

    Changed comments

--- vim/vim_script.sh
+++ vim/vim_script.sh
@@ -22,27 +22,21 @@ clear
 # Creating repo dir and cloning repository
 if [ ! -d ~/repo ] && [ ! -d ~/repo/linux_stuff ]; then
 	echo
-	echo =================================================================
 	echo Creating ~/repo/linux_stuff directory...
 	echo Cloning repository...
-	echo =================================================================
 	mkdir -p ~/repo
 	mkdir ~/repo/linux_stuff
 	cd ~/repo/linux_stuff
 	git clone https://github.com/micdud1995/linux_stuff.git
 else
 	echo
-	echo =================================================================
 	echo OK, ~/repo/linux_stuff already exists...
-	echo =================================================================
 fi
 #=================================================================
 
 #=================================================================
 echo
-echo =================================================================
 echo Configuration of vim...
-echo =================================================================
 echo
 #=================================================================
 
