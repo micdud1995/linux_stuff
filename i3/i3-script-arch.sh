78d4aa2b8f2c0822942ba2ced9ea0f91796c46f5
Michal <michal.dudek1995@gmail.com>
2015-07-29 14:17:26 +0200

    Added different i3lock pictures for distros

--- i3/i3-script-arch.sh
+++ i3/i3-script-arch.sh
@@ -9,6 +9,7 @@ if [ ! -d ~/repo ] && [ ! -d ~/repo/linux_stuff ]; then
 	echo Creating ~/repo/linux_stuff directory...
 	echo Cloning repository...
 	mkdir -p ~/repo
+	mkdir -p ~/Pictures
 	mkdir ~/repo/linux_stuff
 	cd ~/repo/linux_stuff
 	git clone https://github.com/micdud1995/linux_stuff.git
@@ -30,4 +31,4 @@ sudo pacman -S i3 dmenu lxrandr pavucontrol lxterminal nitrogen feh
 # Copying config files from repo 
 cp ~/repo/linux_stuff/i3/hide.i3status.conf ~/.i3status.conf
 cp ~/repo/linux_stuff/i3/config ~/.i3/config
-cp ~/repo/linux_stuff/i3/i3lock.png ~/tmp/i3lock.png
+cp ~/repo/linux_stuff/i3/i3lock-arch.png ~/Pictures/i3lock-arch.png
