deb1f76e09a748998e06caf6277810676bb48050
Michal Dudek <dud95@gmx.us>
2015-10-16 16:55:57 +0200

    Changed default terminal to xterm

--- i3/load_workspaces.sh
+++ i3/load_workspaces.sh
@@ -1,19 +1,19 @@
-i3-msg 'workspace 5: IRC; append_layout /home/michal/.i3/workspace_5.json'
-(lxterminal -e weechat &)
+# i3-msg 'workspace 5: IRC; append_layout /home/michal/.i3/workspace_5.json'
+# (lxterminal -e weechat &)
 
-sleep 3
+# sleep 3
 
-i3-msg 'workspace 3: Code; append_layout /home/michal/.i3/workspace_3.json'
+i3-msg "workspace 3: Code; append_layout /home/michal/.i3/workspace_3.json"
 (lxterminal -e vim &)
 
 sleep 3
 
-i3-msg 'workspace 1: Desktop; append_layout /home/michal/.i3/workspace_1.json'
-# (xterm -e top &)
-# (xterm -e "sh -c 'export TERM="screen-256color" && ranger'" &)
-# (xterm -e mocp &)
-# (xterm &)
-(lxterminal -e mocp &)
-(lxterminal -e top &)
-(lxterminal -e "sh -c 'export TERM="screen-256color" && ranger'" &)
-(lxterminal -e &)
+i3-msg "workspace 1: Desktop; append_layout /home/michal/.i3/workspace_1.json"
+(xterm -e top &)
+(xterm -e "sh -c 'export TERM="screen-256color" && ranger'" &)
+(xterm -e mocp &)
+(xterm &)
+# (lxterminal -e mocp &)
+# (lxterminal -e htop &)
+# (lxterminal -e "sh -c 'export TERM="screen-256color" && ranger'" &)
+# (lxterminal -e &)
