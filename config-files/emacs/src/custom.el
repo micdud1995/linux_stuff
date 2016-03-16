b0964c44fb1f72e6a1e32f0429e3bb03a49e44e5
micdud1995 <michal.dudek@gmx.com>
2016-03-16 19:21:26 +0100

    Emacs: changed ecb layout

--- config-files/emacs/src/custom.el
+++ config-files/emacs/src/custom.el
@@ -1,71 +1,34 @@
-;; Create repositories cache, if required
-(when (not package-archive-contents)
-  (package-refresh-contents))
-
-;; Declare a list of required packages
-(defvar super-emacs--required-packages
-  '(async
-    auto-complete
-    autopair
-    bongo
-    centered-cursor-mode
-    dash
-    ecb
-    elfeed
-    git-commit
-    magit
-    magit-popup
-    popup
-    quasi-monochrome-theme
-    smex
-    switch-window
-    undo-tree
-    with-editor))
-
-;; Install required packages
-(mapc (lambda (p)
-    (package-install p))
-      super-emacs--required-packages)
-
-;; Cursor always centered
-(and
-  (require 'centered-cursor-mode)
-  (global-centered-cursor-mode +1))
-
-;; Install required packages
-(mapc (lambda (p)
-   (package-install p))
-   super-emacs--required-packages)
-
-;; InteractivelyDoThings
-(ido-mode)
-(ido-mode 'both) ;; for buffers and files
-ido-case-fold  t ;; be case-insensitive
-(setq confirm-nonexistent-file-or-buffer nil)
-
-;; Winner-mode
-(winner-mode t)
-(windmove-default-keybindings)
-
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(ansi-color-names-vector
    ["#242424" "#e5786d" "#95e454" "#cae682" "#8ac6f2" "#333366" "#ccaa8f" "#f6f3e8"])
  '(calendar-abbrev-length 12)
  '(calendar-date-style (quote european))
+ '(custom-safe-themes
+   (quote
+    ("1fab355c4c92964546ab511838e3f9f5437f4e68d9d1d073ab8e36e51b26ca6a" default)))
  '(ecb-after-directory-change-hook nil)
  '(ecb-auto-activate t)
  '(ecb-auto-expand-directory-tree (quote best))
  '(ecb-compile-window-height 6)
+ '(ecb-create-layout-frame-height 40)
+ '(ecb-create-layout-frame-width 25)
+ '(ecb-create-new-layout right)
  '(ecb-eshell-auto-activate t)
  '(ecb-fix-window-size (quote width))
- '(ecb-layout-name "leftright1")
+ '(ecb-layout-name "leftright2")
  '(ecb-layout-window-sizes nil)
  '(ecb-minor-mode-text "")
  '(ecb-options-version "2.40")
+ '(ecb-source-path (quote (("/home/michal/.emacs.d/src/" "/"))))
+ '(ecb-tip-of-the-day nil)
  '(ecb-windows-height 40)
- '(ecb-windows-width 32)
+ '(ecb-windows-width 20)
  '(erc-autojoin-mode t)
  '(erc-beep-match-types (quote (current-nick)))
  '(erc-beep-p t)
@@ -88,8 +51,15 @@ ido-case-fold  t ;; be case-insensitive
  '(erc-track-mode t)
  '(european-calendar-style t)
  '(fci-rule-color "#383838")
+ '(ido-ignore-buffers
+   (quote
+    ("*ECB Methods*" "*ECB Sources*" "*Messages*" "^ " "\\` ")))
  '(inhibit-startup-echo-area-message nil)
  '(inhibit-startup-screen t)
+ '(neo-theme (quote ascii))
+ '(neo-window-fixed-size t)
+ '(neo-window-position (quote left))
+ '(neo-window-width 20)
  '(nrepl-message-colors
    (quote
     ("#CC9393" "#DFAF8F" "#F0DFAF" "#7F9F7F" "#BFEBBF" "#93E0E3" "#94BFF3" "#DC8CC3")))
@@ -118,31 +88,10 @@ ido-case-fold  t ;; be case-insensitive
      (340 . "#94BFF3")
      (360 . "#DC8CC3"))))
  '(vc-annotate-very-old-color "#DC8CC3"))
-
-;; Autocomplete
-(ac-config-default)
-
-;; Autostart autopair
-(autopair-global-mode)
-
-;; Undo things
-(global-undo-tree-mode)
-(global-set-key (kbd "M-/") 'undo-tree-visualize)
-
 (custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
  '(cursor ((t nil)))
  '(ecb-default-highlight-face ((t nil))))
-
-(setq ecb-tip-of-the-day nil)
-(ecb-activate)
-
-(setq elfeed-feeds
-    '("https://www.archlinux.org/feeds/news/"
-      "https://www.archlinux.org/feeds/releases/"
-      "http://bash.org.pl/rss"
-      "http://wroclaw.naszemiasto.pl/rss/artykuly/61.xml"
-      "http://wroclaw.naszemiasto.pl/rss/artykuly/73.xml"
-      "http://feeds.linuxportal.pl/LinuxPortalpl-news" 
-      "http://joemonster.org/backend.php?channel" 
-      "http://www.cdaction.pl/rss_newsy.xml"
-      "http://www.devrand.org/feeds/posts/default"))
